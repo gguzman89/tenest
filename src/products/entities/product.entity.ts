25daaec90adea6ead8e86db16a7393bd22abd44b
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-05-05 12:51:23 -0300

    feat: enable relations & adapter result

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from "./product-image.entity";
 
 
 
@@ -49,7 +50,13 @@ export class Product {
     default: []
   })
   tags: string[];
-  // images
+
+  @OneToMany(
+    () => ProductImage,
+    (productImage) => productImage.product,
+    { cascade: true, eager: true }
+  )
+  images?: ProductImage[];
 
   @BeforeInsert()
   @BeforeUpdate()
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,10 +3,10 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 import { InjectRepository } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { PaginationDTO } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid'
+import { ProductImage, Product } from './entities';
 
 @Injectable()
 export class ProductsService {
@@ -16,18 +16,25 @@ export class ProductsService {
   constructor(
 
     @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>
+    private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>
 
   ) { }
 
   async create(createProductDto: CreateProductDto) {
 
     try {
+      const { images = [], ...productDetails } = createProductDto; // operator rest ...
 
-      const product = this.productRepository.create(createProductDto);
+      const product = this.productRepository.create({
+        ...productDetails, // operator spreet ...
+        images: images.map(image => this.productImageRepository.create({ url: image }))
+      });
       await this.productRepository.save(product);
 
-      return product;
+      return { ...product, images };
     } catch (error) {
       this.handleDBExceptions(error)
     }
@@ -38,14 +45,21 @@ export class ProductsService {
 
     const { limit = 10, offset = 5 } = paginationDto
 
-    return await this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
-      //TODO: relaciones
+      relations: {
+        images: true
+      },
       order: {
         slug: 1
       }
     });
+
+    return products.map(product => ({
+      ...product,
+      images: product.images.map(img => img.url)
+    }))
   }
 
   async findOne(term: string) {
@@ -58,12 +72,14 @@ export class ProductsService {
       product = await this.productRepository.findOneBy({ id: term })
     } else {
       // product = await this.productRepository.findOneBy({ slug: term })
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder('prod');
       product = await queryBuilder
         .where(`LOWER(title) =:title or slug =:slug`, {
           title: term.toLowerCase(),
           slug: term.toLowerCase()
-        }).getOne();
+        })
+        .leftJoinAndSelect('prod.images', 'prodImages')
+        .getOne();
 
     }
 
@@ -73,11 +89,20 @@ export class ProductsService {
     return product;
   }
 
+  async findOnePlain(term: string) {
+    const { images = [], ...rest } = await this.findOne(term);
+    return {
+      ...rest,
+      images: images.map(img => img.url)
+    }
+  }
+
   async update(id: string, updateProductDto: UpdateProductDto) {
 
     const product = await this.productRepository.preload({
       id: id,
-      ...updateProductDto
+      ...updateProductDto,
+      images: []
     });
 
     if (!product) throw new NotFoundException(`Product with id: ${id} not found`);
