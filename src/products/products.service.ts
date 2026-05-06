f6aa7c458b59dba04f4fd5dd0530ad7612252355
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-05-06 11:55:38 -0300

    feat: delete enable cascade

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,7 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PaginationDTO } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid'
 import { ProductImage, Product } from './entities';
@@ -19,7 +19,9 @@ export class ProductsService {
     private readonly productRepository: Repository<Product>,
 
     @InjectRepository(ProductImage)
-    private readonly productImageRepository: Repository<ProductImage>
+    private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly datasource: DataSource,
 
   ) { }
 
@@ -99,19 +101,41 @@ export class ProductsService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
 
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: []
-    });
+    const { images, ...toUpdate } = updateProductDto;
+
+    const product = await this.productRepository.preload({ id, ...toUpdate });
 
     if (!product) throw new NotFoundException(`Product with id: ${id} not found`);
 
+    // Create Query Runner
+    const queryRunner = this.datasource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
 
-      await this.productRepository.save(product)
-      return product;
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, { product: { id } });
+
+        product.images = images.map(
+          image => this.productImageRepository.create({ url: image })
+        )
+      } else {
+        // ???
+      }
+
+      await queryRunner.manager.save(product);
+      // await this.productRepository.save(product)
+
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      // return product;
+      return this.findOnePlain(id);
     } catch (error) {
+
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
 
       this.handleDBExceptions(error)
     }
@@ -135,5 +159,21 @@ export class ProductsService {
     this.logger.error(error); // log inside nosotros
     // console.log(error);
     throw new InternalServerErrorException('Unexpected error, check server logs')
+  }
+
+  async deleteAllProducts() {
+
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+
+      return await query
+        .delete()
+        .where({})
+        .execute();
+    } catch (error) {
+
+      this.handleDBExceptions(error);
+    }
   }
 }
