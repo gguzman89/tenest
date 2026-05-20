3f309eebeb8d7f82f6452387dccc6fc2bdf7560a
gabriel guzman <ggtpi0gabriel@gmail.com>
2026-05-20 18:10:32 -0300

    feat: Config swagger UI

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
-import { FilesService } from './files.service';
+import { ConfigService } from '@nestjs/config';
+import { ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
+
+import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
 import { type Response } from 'express';
-import { ConfigService } from '@nestjs/config';
 
 
+@ApiTags('Files - Get and Upload')
 @Controller('files')
 export class FilesController {
   constructor(
